9e3a5f3f88bc2d854734a6e00eea7185578bb503
2021180031 <2021180031@tukorea.ac.kr>
2025-09-22 12:41:56 +0900

    우선 주어진 스프라이트시트를 기준으로 캐릭터가 가운데서만 달리게 코드를 수정한다. 또한 다음 프레임을 결정하는 함수와 캐릭터를 그리는 함수로 코드를 분리하였다.

--- character_runs.py
+++ character_runs.py
@@ -5,14 +5,25 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
-frame=0
-for x in range(0,800,10):
+GRASS_LEVEL=90
+
+
+
+
+def draw_character(frame):
     clear_canvas()
-    grass.draw(400,30)
-    character.clip_draw(frame*100,0,100,100,x,90,200,200)
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 0, 100, 100, 400, GRASS_LEVEL)
     update_canvas()
-    frame = (frame+1)%8
     delay(0.05)
 
-close_canvas()
 
+def next_frame(frame):
+    return (frame + 1) % 8
+
+frame=0
+while True:
+   draw_character(frame)
+   frame=next_frame(frame)
+
+close_canvas()
