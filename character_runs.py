6cde2132f24a52565eb195c7e5ac4f04192a787f
2021180031 <2021180031@tukorea.ac.kr>
2025-09-22 18:25:14 +0900

    코드 재사용을 위해 함수에 새로운 인수 Clipped_height 선언 주어진 프레임과 원시적으로 정의된 애니메이션 코드를 받아서 화면에 그림을 그린다.

--- character_runs.py
+++ character_runs.py
@@ -10,10 +10,10 @@ character = load_image('animation_sheet_final_final.png')
 
 
 #correction: "forth" animation works when y=400
-def draw_character(frame):
+def draw_character(frame,Clipped_height):
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 120, 400, 120, 120, 400, 250,400,400)
+    character.clip_draw(frame * 120, Clipped_height, 120, 120, 400, 250,400,400)
     update_canvas()
     delay(0.05)
 
@@ -23,7 +23,7 @@ def next_frame(frame):
 
 frame=0
 while True:
-   draw_character(frame)
+   draw_character(frame,0)
    frame=next_frame(frame)
 
 close_canvas()
